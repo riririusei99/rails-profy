95199bb807b7050ddea4119d3eb3a78a853482de
riririusei99 <riry1313@gmail.com>
2019-05-01 08:52:04 +0000

    add association

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,6 @@
 class Group < ApplicationRecord
+
+  # association
   has_many :users
+  has_many :questions, ->{ order("created_at DESC") } 
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,6 @@
 class Question < ApplicationRecord
+
+  # association
+  belongs_to :user
+  belongs_to :group
 end
