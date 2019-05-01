6b3815c80e1f30c6b9ef5d6e49f4c82e8016a17d
riririusei99 <riry1313@gmail.com>
2019-05-01 08:55:30 +0000

    add validation

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,8 @@ class Group < ApplicationRecord
   # association
   has_many :users
   has_many :questions, ->{ order("created_at DESC") } 
+  
+   #validation
+  validates_presence_of :user_id, :text, :group_id
+
 end
