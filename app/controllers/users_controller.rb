0ef09475ffef04033c936d9c66e240c0379f9e80
riririusei99 <riry1313@gmail.com>
2019-05-01 04:42:49 +0000

    add update action to userController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,12 @@ class UsersController < ApplicationController
   end
 
   def update
+    current_user.update(update_params)
+  end
+  
+  private
+  def update_params
+    params.require(:user).permit(:family_name, :first_name, :family_name_kana, :first_name_kana)
   end
   
 end
