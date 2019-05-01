e514f46e6e3ee8d7efe33eb7f49710e57accad42
riririusei99 <riry1313@gmail.com>
2019-05-01 10:10:04 +0000

    add show action to questionController

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,6 +5,11 @@ class QuestionsController < ApplicationController
     redirect_to :root and return
   end
 
+  def show
+    @question = Question.find(params[:id])
+    @answers = @question.answers
+  end
+
   private
   def create_params
     params.require(:question).permit(:text).merge(user_id: current_user.id, group_id: current_user.group_id)
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to:'top#index'
   resources :users, only: [:show, :edit, :update]
   resources :groups, only: [:show, :edit, :update]
-  resources :questions, only: [:create]
+  resources :questions, only: [:create, :show]
   resources :answers, only: [:new, :create]
 end
