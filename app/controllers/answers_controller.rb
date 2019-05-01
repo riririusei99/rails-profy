745e029db1f3b5b79dc22fd3b47a9f4b7d832730
riririusei99 <riry1313@gmail.com>
2019-05-01 09:33:09 +0000

    rails g controller answers

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,9 @@
+class AnswersController < ApplicationController
+
+  def new
+    @question = Question.find(params[:question_id])
+    @answer = Answer.new
+    @answer.question_id = @question.id
+  end
+
+end
