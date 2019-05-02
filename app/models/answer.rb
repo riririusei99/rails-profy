8b16f0570734bfc790c035cce556ce207319ce03
riririusei99 <riry1313@gmail.com>
2019-05-02 02:49:06 +0000

    change model for polymophic relationship

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,5 @@
 class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
+  has_one :user, as: :content, dependent: :destroy
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,8 +3,9 @@ class Question < ApplicationRecord
   # association
   belongs_to :user
   belongs_to :group
-  has_many :answers
-  # has_many :answers, dependent: :destroy
+  has_many :answers, dependent: :destroy
+  has_one :feed_content, as: :content, dependent: :destroy
+
 
   #validation
   # validates_presence_of :user_id, :text, :group_id
