cfb879b2e632a0ea1e811c0ae604bcaba0ac0dc5
riririusei99 <riry1313@gmail.com>
2019-05-02 03:52:27 +0000

    add @feed_contents to topController

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,7 +3,8 @@ class Group < ApplicationRecord
   # association
   has_many :users
   has_many :questions, ->{ order("created_at DESC") } 
-  
+  has_many :feed_contents, ->{ order("updated_at DESC") }
+
    #validation
   validates_presence_of :user_id, :text, :group_id
 
